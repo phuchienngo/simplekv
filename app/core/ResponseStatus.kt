fc8f545a1395ec892793a2681ec6e6b1593b456d
chien.ngo1 <chien.ngo1@mservice.com.vn>
2025-05-01 19:28:15 +0700

    add swiss table

--- app/core/ResponseStatus.kt
+++ app/core/ResponseStatus.kt
@@ -1,17 +1,17 @@
 package app.core
 
-enum class ResponseStatus(val value: Short) {
-  NoError(0x0000),
-  KeyNotFound(0x0001),
-  KeyExists(0x0002),
-  ValueTooLarge(0x0003),
-  InvalidArguments(0x0004),
-  ItemNotStored(0x0005),
-  IncrDecrOnNonNumericValue(0x0006),
-  UnknownCommand(0x0081),
-  OutOfMemory(0x0082),
-  NotSupported(0x0083),
-  InternalError(0x0084),
-  Busy(0x0085),
-  TemporaryFailure(0x0086),
+enum class ResponseStatus(val code: Short, val description: String) {
+  NoError(0x0000, "No error"),
+  KeyNotFound(0x0001, "Key not found"),
+  KeyExists(0x0002, "Key exists"),
+  ValueTooLarge(0x0003, "Value too large"),
+  InvalidArguments(0x0004, "Invalid arguments"),
+  ItemNotStored(0x0005, "Item not stored"),
+  IncrDecrOnNonNumericValue(0x0006, "Incr/Decr on non-numeric value"),
+  UnknownCommand(0x0081, "Unknown command"),
+  OutOfMemory(0x0082, "Out of memory"),
+  NotSupported(0x0083, "Not supported"),
+  InternalError(0x0084, "Internal error"),
+  Busy(0x0085, "Busy"),
+  TemporaryFailure(0x0086, "Temporary failure"),
 }
